c95b506f2e83585e9940c5d73b80db4300847596
MadeleineHarbom <madeleine.harbom@gmail.com>
2019-03-05 09:15:12 +0100

    server igen igen iegn

--- src/RecieveThread.java
+++ src/RecieveThread.java
@@ -1,27 +1,32 @@
-package src;
+//package src;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 
 public class RecieveThread extends Thread {
 
-    BufferedReader incoming;
+    private BufferedReader sender;
+    private String sendername;
 
-    public RecieveThread(BufferedReader bf) {
-        this.incoming = bf;
+
+
+    public RecieveThread (BufferedReader bf, String countername) {
+        this.sender = bf;
+        this.sendername = countername;
     }
 
-    @Override
     public void run() {
-
-
-
-        //request player information
-
         while (true) {
+            try {
+                String s = sender.readLine();
+                System.out.println("FROM " + this.sendername +": "  + s);
+            }
+            catch (IOException e) {
+                System.out.println("IO exception");
+            }
 
         }
     }
-
 
 
 
