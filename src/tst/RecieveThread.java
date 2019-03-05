9e44466fe111d5279dc43eb495267dde0417a59c
MadeleineHarbom <madeleine.harbom@gmail.com>
2019-03-05 09:13:04 +0100

    Fra Made

---
+++ src/tst/RecieveThread.java
@@ -0,0 +1,40 @@
+package tst;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class RecieveThread extends Thread {
+
+    //BufferedReader incoming;
+/*
+    public RecieveThread(BufferedReader bf) {
+        this.incoming = bf;
+    }*/
+
+    private BufferedReader sender;
+    private String sendername;
+
+
+
+    public RecieveThread (BufferedReader bf, String countername) {
+        this.sender = bf;
+        this.sendername = countername;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                String s = sender.readLine();
+                System.out.println("FROM " + this.sendername +": "  + s);
+            }
+            catch (IOException e) {
+                System.out.println("IO exception");
+            }
+
+        }
+    }
+
+
+
+
+}
